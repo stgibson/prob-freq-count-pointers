021eec8731688fc2ab4f6e19a3a7b5280f89c08c
Sean Gibson <seanthomasgibson@gmail.com>
2021-05-04 23:48:40 -0700

    Solved two array object problem. Implemented twoArrayObject(arr1, arr2) in two-array-object.js.

--- two-array-object.js
+++ two-array-object.js
@@ -1,3 +1,18 @@
 // add whatever parameters you deem necessary
-function twoArrayObject() {
+function twoArrayObject(arr1, arr2) {
+  const newObj = {};
+  // for each item in arr1
+  for (let i = 0; i < arr1.length; i++) {
+    // if no corresp item in arr2, add item to obj with val null
+    if (i >= arr2.length) {
+      newObj[arr1[i]] = null;
+    }
+    // else add item to obj with corresp item in arr2 as val
+    else {
+      newObj[arr1[i]] = arr2[i];
+    }
+  }
+
+  // return new obj
+  return newObj;
 }
