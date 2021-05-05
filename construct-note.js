c42053fa4c34a260c5221248df3c393a5d015609
Sean Gibson <seanthomasgibson@gmail.com>
2021-05-04 22:32:23 -0700

    Solved constructNote problem. Added getFrequencies(str) & implemented constructNote(msg, letters) in construct-note.js.

--- construct-note.js
+++ construct-note.js
@@ -1,3 +1,30 @@
+function getFrequencies(str) {
+  const freq = {};
+  for (let char of str) {
+    freq[char] = (freq[char] + 1) || 1;
+  }
+  return freq;
+}
+
 // add whatever parameters you deem necessary
-function constructNote() {
+function constructNote(msg, letters) {
+  // create 2 objects that store frequency of chars in msg and letters
+  const msgCharFreq = getFrequencies(msg);
+  const lettersCharFreq = getFrequencies(letters);
+
+  // for each letter in freq. obj for msg
+  for (let char in msgCharFreq) {
+    // if it isn't in freq. object for letter
+    if (!lettersCharFreq[char]) {
+      // can't construct note, so return false
+      return false;
+    }
+    // if its value is greater than its value in freq. obj for letters
+    if (msgCharFreq[char] > lettersCharFreq[char]) {
+      // can't construct note, so return false
+      return false;
+    }
+  }
+  // can construct note, so return true
+  return true;
 }
