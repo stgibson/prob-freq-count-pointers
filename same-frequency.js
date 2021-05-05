84d2af0c39afeb064d98080b6712e69c2c48a23a
Sean Gibson <seanthomasgibson@gmail.com>
2021-05-05 00:17:46 -0700

    Solved same frequency problem. Added getFreqOfDigits(num) & implemented sameFrequency(num1, num2) in same-frequency.js.

--- same-frequency.js
+++ same-frequency.js
@@ -1,3 +1,31 @@
+function getFreqOfDigits(num) {
+  const freqOfDigits = {};
+  while (num > 0) {
+    const digit = num % 10;
+    freqOfDigits[digit] = (freqOfDigits[digit] + 1) || 1;
+    num = Math.floor(num / 10);
+  }
+  return freqOfDigits;
+}
+
 // add whatever parameters you deem necessary
-function sameFrequency() {
+function sameFrequency(num1, num2) {
+  // create 2 freq obj of digits for each num
+  const freqNum1 = getFreqOfDigits(num1);
+  const freqNum2 = getFreqOfDigits(num2);
+
+  // for each unique digit in 1st freq obj
+  for (let digit in freqNum1) {
+    // if not in 2nd freq obj, return false
+    if (!freqNum2[digit]) {
+      return false;  
+    }
+    // if val from 1st freq obj !== val from 2nd freq obj, return false
+    if (freqNum1[digit] !== freqNum2[digit]) {
+      return false;
+    }
+  }
+
+  // all digits matched in frequency, so return true
+  return true;
 }
