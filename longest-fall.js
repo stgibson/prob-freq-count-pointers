94074a749e3e65250caaf9a24734e61193aa1044
Sean Gibson <seanthomasgibson@gmail.com>
2021-05-07 16:10:58 -0700

    Implemented longestFall(nums) in longest-fall.js.

--- longest-fall.js
+++ longest-fall.js
@@ -1,3 +1,27 @@
 // add whatever parameters you deem necessary
-function longestFall() {
+function longestFall(nums) {
+  // keep track if longest sequence found thus far
+  let longestSeqLen = 0;
+  // keep track if length of curr sequence
+  let currSeqLen = 0;
+
+  // for each num
+  let prevNum = Number.MIN_VALUE;
+  for (let num of nums) {
+    // if num < previous num, increment curr sequence
+    if (num < prevNum) {
+      currSeqLen++;
+    }
+    // else, update longest sequence & reset curr sequence to 1
+    else {
+      longestSeqLen = Math.max(longestSeqLen, currSeqLen);
+      currSeqLen = 1;
+    }
+    prevNum = num;
+  }
+  // update longest sequence to max of itself and curr sequence
+  longestSeqLen = Math.max(longestSeqLen, currSeqLen);
+
+  // return longest sequence length
+  return longestSeqLen;
 }
