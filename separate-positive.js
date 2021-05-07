286f72538c2de0aac45472e6715aa4d30c4edd88
Sean Gibson <seanthomasgibson@gmail.com>
2021-05-06 19:14:24 -0700

    Solved separatePositive problem. Added swap(arr, i, j) & separatePositive(arr) to separate-positive.js.

--- separate-positive.js
+++ separate-positive.js
@@ -1,3 +1,22 @@
+function swap(arr, i, j) {
+  [arr[i], arr[j]] = [arr[j], arr[i]];
+}
+
 // add whatever parameters you deem necessary
-function separatePositive() {
+function separatePositive(arr) {
+  // create a pointer at start of arr
+  let i = 0;
+
+  // for each num in arr
+  for (let j = 0; j < arr.length; j++) {
+    // if positive
+    if (arr[j] > 0) {
+      // swap with num pointed by pointer & increment pointer
+      swap(arr, i, j);
+      i++;
+    }
+  }
+
+  // return arr
+  return arr;
 }
