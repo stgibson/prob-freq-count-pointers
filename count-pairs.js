5e318ef667328ab3bdc067ec97f2b6aceb9e1fdd
Sean Gibson <seanthomasgibson@gmail.com>
2021-05-07 15:21:28 -0700

    Solved count pairs problem. Implemented countPairs(nums, target) in count-pairs.js.

--- count-pairs.js
+++ count-pairs.js
@@ -1,3 +1,21 @@
 // add whatever parameters you deem necessary
-function countPairs() {
+function countPairs(nums, target) {
+  // add nums to set
+  const numsSet = new Set(nums);
+  // init num of pairs to 0
+  let pairs = 0;
+
+  // for each num in set
+  for (let num of numsSet) {
+    // remove num from set
+    numsSet.delete(num);
+    // if target - num is in set
+    if (numsSet.has(target - num)) {
+      // add to num of pairs
+      pairs++;
+    }
+  }
+
+  // return num of pairs
+  return pairs;
 }
